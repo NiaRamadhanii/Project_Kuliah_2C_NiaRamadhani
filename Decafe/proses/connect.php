9475276b59eda2760f02c9b71b80cbfd2b0b5bef
NiaRamadhanii <niaimas9@gmail.com>
2023-11-13 10:51:58 +0700

    commit kedelapan

--- Decafe/proses/connect.php
+++ Decafe/proses/connect.php
@@ -1,5 +1,5 @@
 <?php 
-    $conn = mysqli_connect("localhost","root","","db_decafe");
+    $conn = mysqli_connect("localhost","u837952942.niaraamdhn","niaAwio5","u837952942.niaraamdhn");
     if(!$conn){
         echo "Gagal koneksi";
     }
