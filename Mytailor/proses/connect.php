20f5312b1fca8a3b545a8716f2a2d886f1e92b75
NiaRamadhanii <niaimas9@gmail.com>
2023-12-12 15:21:29 +0700

    revisi

--- Mytailor/proses/connect.php
+++ Mytailor/proses/connect.php
@@ -1,6 +1,6 @@
 <?php 
-    //$conn = mysqli_connect("localhost","u837952942_mytaylor","ae#=$&jk[Mi7","u837952942_mytaylor");
-    $conn = mysqli_connect("localhost","root","","db_mytaylor");
+    $conn = mysqli_connect("localhost","u837952942_mytaylor","ae#=$&jk[Mi7","u837952942_mytaylor");
+    //$conn = mysqli_connect("localhost","root","","db_mytaylor");
     if(!$conn){
         echo "Gagal koneksi";
     }
